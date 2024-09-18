8322075c656db7c640aa29c33df919a31a513b7e
Volodymyr Zhuchenko <vovazhychenko61@gmail.com>
2024-09-19 00:59:53 +0300

    FullProject

---
+++ Fibonachi/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введіть номер числа Фібоначчі: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int fib = Fibonacci(n);
+        Console.WriteLine($"Число Фібоначчі {n} дорівнює {fib}");
+    }
+
+    static int Fibonacci(int n)
+    {
+        if (n <= 1)
+            return n;
+
+        int a = 0, b = 1, temp;
+
+        for (int i = 2; i <= n; i++)
+        {
+            temp = a + b;
+            a = b;
+            b = temp;
+        }
+
+        return b;
+    }
+}
+
